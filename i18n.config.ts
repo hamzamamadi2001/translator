8341b13fb94817cc93c28c162c38636420b763e1
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-05-03 17:24:26 +0200

    first update

---
+++ i18n.config.ts
@@ -0,0 +1,13 @@
+export default defineI18nConfig(nuxt => ({
+    legacy: false,
+    locale: 'en',
+    messages: {
+      en: {
+        welcome: 'Welcome'
+      },
+      fr: {
+        welcome: 'Bienvenue'
+      }
+    }
+  }))
+  
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+    modules: [
+      '@nuxtjs/i18n'
+    ],
+    i18n: {
+      vueI18n: './i18n.config.ts' // if you are using custom path, default 
+    }
+  })
+  
