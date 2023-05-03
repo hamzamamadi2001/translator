939872ea8dfe55d5e8bdc994c4bd84cf1f594456
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-05-03 17:59:46 +0200

    first update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 export default defineNuxtConfig({
+  ssr:false,
     modules: [
       '@nuxtjs/i18n'
     ],
     i18n: {
+        
       vueI18n: './i18n.config.ts' // if you are using custom path, default 
     }
   })
