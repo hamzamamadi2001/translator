3b46f7f4d7288fdd26d96223f0944ea05eb8f288
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-05-05 00:17:59 +0200

    first update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'url'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
+
 export default defineNuxtConfig({
- 
+  vite: {
+        plugins: [
+          VueI18nVitePlugin({
+            include: [
+              resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.json')
+            ]
+          })
+        ]
+      },
+
+
+
     modules: [
       '@nuxtjs/i18n'
     ],
